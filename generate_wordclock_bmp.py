900c92f8c36f8672a3cb5992beec16cf502332db
Michał Leśniewski <mlesniew@gmail.com>
2019-07-24 20:42:05 +0200

    Add Python script, which generates the C bitmaps

---
+++ generate_wordclock_bmp.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+
+MATRIX = '''
+    HALFZERO
+    YQUARTER
+    PASTODOT
+    SIXTWONE
+    THREETEN
+    FIVEIGHT
+    TWELFOUR
+    SEVENINE
+'''.strip()
+
+WORDS = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'el-even',
+    'twel-ve',
+    'a-quarter',
+    'half',
+    'past',
+    'to',
+    'dot'
+]
+
+MATRIX = MATRIX.strip().upper().replace(' ', '')
+WORDS = [w.upper() for w in WORDS]
+
+for idx, word in enumerate(WORDS):
+    name = word.replace('-', '').upper()
+    print(f'#define BMP_{name:10s} (bitmap + {idx*8})')
+
+print()
+print('const unsigned char bitmap[] = {')
+for word_idx, word in enumerate(WORDS):
+    name = word.replace('-', '').upper()
+
+    def iter_indices():
+        parts = word.split('-')
+        idx = 0
+        for part in parts:
+            idx = MATRIX.index(part, idx)
+            assert idx >= 0
+            for s, c in enumerate(part):
+                yield idx + s
+            idx += len(part)
+
+    tab = [[0 for x in range(8)] for y in range(8)]
+
+    for idx in iter_indices():
+        x = idx % 9
+        y = idx // 9
+        tab[y][x] = 1
+
+    t = ' '.join('%3i,' % sum(e << (7 - i) for i, e in enumerate(row)) for row in tab)
+    print(f'    {t}        //  {word_idx * 8:3}  -- {name}')
+print('};')
